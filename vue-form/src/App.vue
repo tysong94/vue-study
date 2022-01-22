17fd32a2be0766f78f9a4a2729752128fc3c0bfb
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-22 14:55:02 +0900

    Vue Beginner Complete

---
+++ vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label> <!-- label은 input을 바라본다. -->
+      <input id="username" type="text" v-model="username"> <!-- 2way ??. element와 script를 연결? -->
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault(); // 새로고침 막기
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users'
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
