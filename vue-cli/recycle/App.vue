59df2f8889da65d73be5637a963adb72a019d500
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-22 14:03:22 +0900

    single file component complete

---
+++ vue-cli/recycle/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 컴포넌트 명명법 종류 -->
+    <!-- 
+    <hello-world></hello-world> : ?? , 파일 바로가기 기능?
+    <HelloWorld></HelloWorld> : PascalCase
+    <HelloWorld/> : PascalCase 축약형 
+    -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './HelloWorld.vue'
+
+export default {
+  // 인스턴스 옵셕 속성 or 컴포넌트 옵션 속성
+  name: 'App',
+  components: {
+    HelloWorld // = 'hello-world': HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 템플릿 루트에는 하나의 element만 있어야 함. -->
+  <div>
+    <!-- <app-header v-bind:하위 컴포넌트의 프롭스 속성 이름="상위컴포넌트의 데이터 이름"></app-header> -->
+    <!-- 방향 : 왼쪽 -->
+    <!-- <app-header v-on:하위 컴포넌트의 이벤트 이름="상위컴포넌트의 메소드 이름"></app-header> -->
+    <!-- 방향 : 오른쪽 -->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
+// 위의 import는 다음과 같다.
+// 저장되는 형식이 객체 -> 파일로 바뀌어짐.
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+
+// new Vue({
+//     data: {
+//         str: 'hi'
+//     }
+// })
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'app-header': AppHeader/* 컴포넌트 이름 : 컴포넌트 변수(파일) */
+  },
+  data: function () {
+    return {
+      str: "123",
+    };
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'renewd';
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
