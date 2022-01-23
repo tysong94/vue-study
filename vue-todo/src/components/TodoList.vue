cbb5bb3a4481fb046115da5e7a3778856db31816
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-23 21:41:39 +0900

    made TodoList & Trash Icon

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,67 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+        {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    removeTodo: function(event) {
+      console.log(event);
+    }
+  },
+  created: function() {
+    if(localStorage.length > 0) {
+      for(var i = 0; i < localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(localStorage.key(i));  
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+ul {
+  list-style-type: none; /* 앞에 o 표시 없애는 것 */
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0; /* 위아레 마진, 왼쪽 오른쪽 마진 */
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
 </style>
