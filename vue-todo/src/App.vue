38ce073083759861a311f2e9431cac6a306ce9ce
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-23 10:53:33 +0900

    vue-todo projtect start

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    // 컴포넌트 태그명 : 컴포넌트 변수(컴포넌트 내용)
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
