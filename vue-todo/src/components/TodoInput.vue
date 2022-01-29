0a0df1a45887ef7f05149e0c692aacc76802b5f4
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-29 14:46:28 +0900

    Modal 태그 수정

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -5,13 +5,13 @@
       <i class="fas fa-plus addBtn"></i>
     </span>
 
-    <modal v-if="showModal" @close="showModal = false">
+    <Modal v-if="showModal" @close="showModal = false">
     <!--
     you can use custom content here to overwrite
     default content
     -->
     <h3 slot="header">custom header</h3>
-    </modal>
+    </Modal>
   </div>
 </template>
 
