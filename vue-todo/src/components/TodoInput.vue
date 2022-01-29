99a271016de48b8d365e21c5e84461320f6694a1
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-29 14:45:03 +0900

    모달 컴포넌트 등록

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,15 +4,26 @@
     <span class="addContainer" v-on:click="addTodoItem">
       <i class="fas fa-plus addBtn"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+    <!--
+    you can use custom content here to overwrite
+    default content
+    -->
+    <h3 slot="header">custom header</h3>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 // v-model = 2-way binding.(template <-> script)
 export default {
   data: function() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     }
   },
   methods: {
@@ -26,6 +37,9 @@ export default {
     clearInput: function() {
       this.newTodoItem = '';
     }
+  },
+  components: {
+    Modal: Modal
   }
 }
 </script>
