4fc3a8774d95a2071ffb2e5426287f9345895721
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-01-30 12:43:53 +0900

    [리팩토링] state 속성 적용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -4,6 +4,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex); // 전역으로 플러그인을 사용
 
-export const store = new Vuex.Store({
+const storage = {
+  fetch() {
+    const arr = [];
+    if(localStorage.length > 0) {
+      for(let i = 0; i < localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          const todoItemObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+          arr.push(todoItemObj);
+        }
+      }
+    }
+    return arr;
+  }
+}
 
+export const store = new Vuex.Store({
+  state: {
+    todoItems: storage.fetch()
+  }
 });
