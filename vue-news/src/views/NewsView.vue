cab72b3741208635aa4918e13cc8e7b240a251ec
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-02-03 23:56:07 +0900

    axios 설치, api 호출

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    news
+    <div v-for="item in news">{{ item.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      news: []
+    }
+  },
+  created() {
+    var vm = this; //axios 내에서 this는 위의 코드를 보고 있지 않음.
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+      .then(response => {
+        console.log(response);
+        vm.news = response.data;
+      })
+      .catch(error => console.log(error));
+  }
 }
 </script>
 
