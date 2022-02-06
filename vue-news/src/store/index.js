829e428994c1d5b9577331909a33360a8bfd206d
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-02-06 23:09:05 +0900

    ItemView 스타일링, v-html 디렉티브

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -23,6 +23,9 @@ export const store = new Vuex.Store({
     fetchedJobs(state) {
       return state.jobs;
     },
+    fetchedItem(state) {
+      return state.item;
+    }
   },
   mutations,
   actions,
--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <p>title : {{ this.$store.state.item.title }}</p>
-    <p>content : {{ this.$store.state.item.content }}</p>
-    <p>user : {{ this.$store.state.item.user }}</p>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${fetchedItem.user}`">
+            {{ fetchedItem.user }}
+          </router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
+      </div>
+      <h2>{{ fetchedItem.title }}</h2>
+    </section>
+    <section>
+      <div v-html="fetchedItem.content"></div>
+    </section>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+  computed: {
+    ...mapGetters(['fetchedItem']),
+  },
   created() {
     const id = this.$route.params.id;
     this.$store.dispatch('FETCH_ITEM', id);
@@ -15,6 +36,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 
 </style>
