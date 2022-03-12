ac5512e660c0434a1f720d2209a44177f6b0211b
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-13 00:45:02 +0900

    axio api 설정 공통화

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 // 1. HTTP Request & Response 관련 기본 설정
-const config = {
-	baseUrl: 'http://localhost:3000',
-};
+const instance = axios.create({
+	baseURL: 'http://localhost:3000',
+});
 
 // 2. API 함수들 정리
 function postSignup(signupData) {
-	return axios.post(`${config.baseUrl}/signup`, signupData);
+	// return axios.post(`${config.baseUrl}/signup`, signupData);
+	return instance.post('/signup', signupData);
 }
 
 export { postSignup };
