1eafaddc59a039694e1a5450c6147e4bab4dd484
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-12 21:42:26 +0900

    초기 진입 페이지 설정(redirect)

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -6,6 +6,10 @@ Vue.use(VueRouter);
 export default new VueRouter({
 	routes: [
 		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
 			path: '/login',
 			component: () => import('@/views/LoginPage.vue'),
 		},
