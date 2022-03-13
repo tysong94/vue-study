8a4b9643b2ad0b132d2e2d450cea65a8ca530b3c
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-13 09:03:46 +0900

    LoginForm component creation, markup

---
+++ vue-til/src/components/common/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+	<form v-on:submit.prevent="submitForm">
+		<div>
+			<label for="username">id: </label>
+			<input id="username" type="text" v-model="username" />
+		</div>
+		<div>
+			<label for="password">pw : </label>
+			<input id="password" type="text" v-model="password" />
+		</div>
+		<button type="submit">로그인</button>
+		<p>{{ logMessage }}</p>
+	</form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
