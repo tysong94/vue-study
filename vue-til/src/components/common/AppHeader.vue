3c3a1a548e8ecc5ebe016786cea80629bbf7f319
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-19 13:36:52 +0900

    add font, icon cdns and polish css styles, add loading spinner

--- vue-til/src/components/common/AppHeader.vue
+++ vue-til/src/components/common/AppHeader.vue
@@ -1,12 +1,14 @@
 <template>
 	<header>
 		<div>
-			<router-link to="/" class="logo">TIL</router-link>
+			<router-link to="/" class="logo">
+				TIL
+				<span v-if="isUserLogin">by {{ $store.state.username }}</span>
+			</router-link>
 		</div>
 		<div class="navigations">
 			<template v-if="isUserLogin">
-				<span class="username">{{ $store.state.username }}</span>
-				<a href="javascript:;" @click="logout">Logout</a>
+				<a href="javascript:;" @click="logout" class="logout-button">Logout</a>
 			</template>
 			<template v-else>
 				<router-link to="/login">로그인</router-link> |
@@ -64,6 +66,9 @@ a.logo {
 	position: fixed;
 	top: 0;
 	width: 100%;
+}
+.logout-button {
+	font-size: 14px;
 }
 a.router-link-exact-active {
 	color: white;
