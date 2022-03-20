724ba4caed306a1e4cf9b7865010323caaab3e68
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-20 22:48:34 +0900

    로딩 스피너 등록을 통한 학습 노트 데이터 로딩 상태 표시

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="main list-container contents">
 		<h1 class="page-header">Today I Learned</h1>
-		<ul>
+		<LoadingSpinner v-if="isLoading"></LoadingSpinner>
+		<ul v-else>
 			<PostListItem
 				v-for="postItem in postItems"
 				v-bind:key="postItem._id"
@@ -14,20 +15,25 @@
 
 <script>
 import { fetchPosts } from '@/api/index';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import PostListItem from '@/components/posts/PostListItem.vue';
 
 export default {
 	components: {
+		LoadingSpinner,
 		PostListItem,
 	},
 	data() {
 		return {
 			postItems: [],
+			isLoading: false,
 		};
 	},
 	methods: {
 		async fetchNotes() {
+			this.isLoading = true;
 			const { data } = await fetchPosts();
+			this.isLoading = false;
 			console.log(data.posts);
 			this.postItems = data.posts;
 		},
