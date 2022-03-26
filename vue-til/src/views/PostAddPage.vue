d21280652249906c338d3976be76cbf4b5459c3f
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-26 19:01:35 +0900

    TIL Create Page HTML Markup and CSS Styling

--- vue-til/src/views/PostAddPage.vue
+++ vue-til/src/views/PostAddPage.vue
@@ -1,11 +1,17 @@
 <template>
-	<div>
-		<h1>add page</h1>
+	<div class="form-container">
+		<PostAddForm></PostAddForm>
 	</div>
 </template>
 
 <script>
-export default {};
+import PostAddForm from '@/components/posts/PostAddForm.vue';
+
+export default {
+	components: {
+		PostAddForm,
+	},
+};
 </script>
 
 <style></style>
