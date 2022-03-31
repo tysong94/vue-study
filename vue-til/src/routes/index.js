6bc572d08a6ff1f9e8c7e1a4877016d743ae5f76
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-03-31 23:04:41 +0900

    로그인되지 않은 사용자 접근 제어 Controlling access for unauthorized users

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
@@ -21,17 +22,17 @@ const router = new VueRouter({
 		{
 			path: '/main',
 			component: () => import('@/views/PostListPage.vue'),
-			meta: { auth: true },
+			meta: { loginRequired: true },
 		},
 		{
 			path: '/add',
 			component: () => import('@/views/PostAddPage.vue'),
-			meta: { auth: true },
+			meta: { loginRequired: true },
 		},
 		{
 			path: '/post/:id',
 			component: () => import('@/views/PostEditPage.vue'),
-			meta: { auth: true },
+			meta: { loginRequired: true },
 		},
 		{
 			path: '*',
@@ -42,7 +43,12 @@ const router = new VueRouter({
 
 // to : 이동할 페이지, from : 현재 페이지, next : 이동을 실행하는 함수
 router.beforeEach((to, from, next) => {
-	console.log(to);
+	// 로그인 필요 속성이 true인 페이지에서 로그인이 안되어 있을 경우
+	if (to.meta.loginRequired && !store.getters.isLogin) {
+		console.log('인증이 필요합니다.');
+		next('/login');
+		return; //return을 안하면 아래의 next를 불필요하게 한번 더 실행
+	}
 	next();
 });
 
