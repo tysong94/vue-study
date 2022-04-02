515f2436b16211acf78284fbae194c181a6591b3
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-04-02 11:40:27 +0900

    뷰 컴포넌트 테스트

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,11 +1,14 @@
-import { sum } from './math';
+import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
 
-sum(10, 20);
-
-// 테스트 케이스의 묶음.
-describe('math.js', () => {
-	test('10 + 20 = 30', () => {
-		const result = sum(10, 20);
-		expect(result).toBe(30);
+describe('LoginForm.vue', () => {
+	test('컴포넌트가 마운팅 되면 unsername이 존재& 초기 값으로 설정되어 있어야 함', () => {
+		const instance = new Vue(LoginForm).$mount();
+		// console.log(instance);
+		expect(instance.username).toBe('');
 	});
 });
+
+// new Vue({
+// 	render: h => h(App);
+// }).$mount();
