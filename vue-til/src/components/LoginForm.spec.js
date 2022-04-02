722da0c2b9dbc067a7ad7a0ff276e59103f3c25f
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-04-02 12:05:38 +0900

    로그인 폼의 인풋 요소 관련 테스트 코드 작성

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -3,9 +3,15 @@ import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
 	test('컴포넌트가 마운팅 되면 unsername이 존재& 초기 값으로 설정되어 있어야 함', () => {
-		const wrapper = shallowMount(LoginForm);
+		const wrapper = shallowMount(LoginForm, {
+			data() {
+				return {
+					username: 'test',
+				};
+			},
+		});
 		const usernameInput = wrapper.find('#username');
-		console.log(usernameInput.html());
+		console.log(usernameInput.element._value);
 	});
 });
 
