18fd811ca6381f434e311a9315d0cea0f86dac4a
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-04-02 12:25:47 +0900

    로그인 컴포넌트 첫번째 테스트 코드 작성 : 이메일 유효성 틀릴 시 경고메시지 출력

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-	test('컴포넌트가 마운팅 되면 unsername이 존재& 초기 값으로 설정되어 있어야 함', () => {
+	test('ID가 이메일 형식이 아니면 경고메시지가 출력된다.', () => {
 		const wrapper = shallowMount(LoginForm, {
 			data() {
 				return {
@@ -10,9 +10,8 @@ describe('LoginForm.vue', () => {
 				};
 			},
 		});
-		const usernameInput = wrapper.find('#username');
-		console.log(usernameInput.element._value);
-		console.log(wrapper.vm.isUsernameValid);
+		const warningText = wrapper.find('.warning');
+		expect(warningText.exists()).toBeTruthy();
 	});
 });
 
