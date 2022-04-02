8edd431adefa5043e51cebfa94e5f86d5bd912a7
tysong94 <42729824+tysong94@users.noreply.github.com>
2022-04-02 11:56:40 +0900

    wrapper.find를 이용한 html 요소 검색

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -4,8 +4,8 @@ import LoginForm from './LoginForm.vue';
 describe('LoginForm.vue', () => {
 	test('컴포넌트가 마운팅 되면 unsername이 존재& 초기 값으로 설정되어 있어야 함', () => {
 		const wrapper = shallowMount(LoginForm);
-		// wrapper.vm = instance
-		expect(wrapper.vm.username).toBe('');
+		const usernameInput = wrapper.find('#username');
+		console.log(usernameInput.html());
 	});
 });
 
